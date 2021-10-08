f2448c710da55659cc07e1db3f3739ace7518007
Ben Reinhart <benjreinhart@gmail.com>
2021-10-08 13:23:57 -0700

    Tag tests to make it easy to filter

--- test/kdl_test.exs
+++ test/kdl_test.exs
@@ -37,13 +37,17 @@ defmodule Kdl.KdlTest do
   end
 
   for {file_name, file_contents} <- input_file_map do
+    tag = file_name |> Path.rootname(".kdl") |> String.to_atom()
+
     if Map.has_key?(expected_file_map, file_name) do
+      @tag tag
       test "input/#{file_name}" do
         assert {:ok, decoded} = decode(unquote(file_contents))
         assert {:ok, encoded} = encode(decoded)
         assert unquote(Map.fetch!(expected_file_map, file_name)) == encoded
       end
     else
+      @tag tag
       test "input/#{file_name}" do
         assert {:error, _message} = decode(unquote(file_contents))
       end
